0c19d89a9598751a3ed89ec47716afae54ea5080
Christina <reeserc@wfu.edu>
2024-09-04 09:07:20 -0400

    initial

---
+++ dist/ow-gutenberg.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '7b86c14f3903e4dc189a');
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import stylesheet
+import './ow-style.scss';
+
+//import utilities
+import './util.js';
+
+// import oasis workflow redux store
+import './store/ow-store.js';
+
+// import Oasis Workflow Sidebar
+import './ow-sidebar.js';
